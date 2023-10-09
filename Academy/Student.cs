84b183a4cb3dd5dcf148098ae8d783eaea1b2796
Uliana.29 <Uliana.29@DESKTOP-6DD5K9D>
2023-10-09 15:37:53 +0300

    Encoding

--- Academy/Student.cs
+++ Academy/Student.cs
@@ -53,10 +53,10 @@ namespace Academy
 		public override void Init(string[] values)
 		{
 			base.Init(values);
-			Speciality = values[4];
-			Group = values[5];
-			Rating = Convert.ToDouble(values[6]);
-			Attendance = Convert.ToDouble(values[7]);
+			Speciality = values[4].TrimStart().TrimEnd();
+			Group = values[5].TrimStart().TrimEnd();
+			Rating = Convert.ToDouble(values[6].TrimStart().TrimEnd());
+			Attendance = Convert.ToDouble(values[7].TrimStart().TrimEnd());
 		}
 		public override void Print()
 		{
